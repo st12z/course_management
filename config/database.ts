a5a976011bfda2903031cff0563bc9da4b325c5d
Nguyễn Tiến Thực <134465390+st12z@users.noreply.github.com>
2024-12-15 00:04:03 +0700

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const connect=async()=>{
   try{
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_UR);
     console.log("Connect success");
   }catch(error){
     console.log("Connect fail")
